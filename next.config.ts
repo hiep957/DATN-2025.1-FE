6ba22b1436df945e5a642e2ddc8bb819a9385a94
hiep957 <hiepma80@gmail.com>
2026-01-09 09:35:17 +0700

    update ngrok

--- next.config.ts
+++ next.config.ts
@@ -16,14 +16,6 @@ const nextConfig: NextConfig = {
     ]
 
   },
-  rewrites: async () => {
-    return [
-      {
-        source: '/api-proxy/:path*',
-        destination: 'https://8bf71b615de0.ngrok-free.app/:path*' // Thay đổi thành URL backend của bạn
-      }
-    ]
-  },
   reactStrictMode: false
 };
 
